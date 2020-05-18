0f3f47f29065eaf76e7968f90a3661e2b9dc1d46
swrrt <swrrt.11@gmail.com>
2020-05-18 08:50:50 +0000

    update

--- nexmark_scripts/autorun.sh
+++ nexmark_scripts/autorun.sh
@@ -5,7 +5,7 @@ for cycle in 300 ; do
             for times in 1; do
 		base=`expr ${AVGRATE} - ${range}`
                 echo $cycle $base $range
-                ./runQuery-auto.sh 1 eagle 2 $cycle $base $range
+                ./runQuery-auto.sh 0 eagle 2 $cycle $base $range
             done
     done
 done
